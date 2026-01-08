12d0fa380413092154f802703eac8bb746ca14da
Nate Derbinsky <nate.derbinsky@gmail.com>
2026-01-08 14:35:53 -0500

    created a tests file for later use and a proper main in the app

--- main.py
+++ main.py
@@ -2,4 +2,11 @@
 TODO: A very useful temperature-conversion app.
 """
 
-print("hello, world!")
+def main() -> None:
+    print("hello, world!")
+    name: str = input('what is your name? ')
+
+    print(f"hello { name }")
+
+if __name__ == "__main__":
+    main()
