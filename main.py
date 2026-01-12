17142e80fa94640dd1a01e1b50a315b4389e1341
Nate Derbinsky <nate.derbinsky@gmail.com>
2026-01-12 14:34:27 -0500

    tested greeting and is it cold

--- main.py
+++ main.py
@@ -2,11 +2,36 @@
 TODO: A very useful temperature-conversion app.
 """
 
-def main() -> None:
-    print("hello, world!")
-    name: str = input('what is your name? ')
+# anything below this is cold
+TEMP_THRESHOLD_F: float = 68
 
+def is_it_cold_f(temp_f: float) -> bool:
+    """
+    Determines if the supplied temperature
+    is below a threshold
+
+    Parameters
+    ==========
+    temp_f : float
+        supplied temperature in F
+
+    Returns
+    =======
+    bool
+        True if it is below a threshold
+    """
+    return temp_f < TEMP_THRESHOLD_F
+
+def greet_human() -> None:
+    """
+    Get a name from the keyboard
+    and say hello!
+    """
+    name: str = input('what is your name? ')
     print(f"hello { name }")
+
+def main() -> None:
+    pass
 
 if __name__ == "__main__":
     main()
--- tests.py
+++ tests.py
@@ -1,3 +1,48 @@
 import unittest
+from unittest.mock import patch, Mock
 
-import main
+from main import (
+    greet_human,
+
+    is_it_cold_f,
+    TEMP_THRESHOLD_F
+)
+
+class TestMainApp(unittest.TestCase):
+
+    def test_isitcoldf_freezing(self) -> None:
+        """Tests is_it_cold_f for freezing"""
+        self.assertTrue(is_it_cold_f(32))
+
+    def test_isitcoldf_boiling(self) -> None:
+        """Tests is_it_cold_f for boiling"""
+        self.assertFalse(is_it_cold_f(212))
+
+    def test_isitcoldf_negative(self) -> None:
+        """Tests is_it_cold_f for negative temps"""
+        self.assertTrue(is_it_cold_f(-40))
+
+    def test_isitcoldf_thresh(self) -> None:
+        """Tests is_it_cold_f for threshold"""
+        self.assertFalse(is_it_cold_f(TEMP_THRESHOLD_F))
+
+    def test_isitcoldf_above(self) -> None:
+        """Tests is_it_cold_f for above the threshold"""
+        self.assertFalse(is_it_cold_f(TEMP_THRESHOLD_F+1))
+
+    def test_isitcoldf_below(self) -> None:
+        """Tests is_it_cold_f for below the threshold"""
+        self.assertTrue(is_it_cold_f(TEMP_THRESHOLD_F-1))
+
+    @patch('builtins.input', side_effect=['alice'])
+    @patch('builtins.print')
+    def test_greethuman_alice(self, mock_print: Mock, _: Mock) -> None:
+        """"Test inputting alice to greet_human"""
+        greet_human()
+        expected_calls = [
+            unittest.mock.call("hello alice"),
+        ]
+        mock_print.assert_has_calls(expected_calls)
+
+if __name__ == "__main__":
+    unittest.main()
